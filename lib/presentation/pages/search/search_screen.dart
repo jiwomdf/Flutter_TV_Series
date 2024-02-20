100fc805825abb0ba9ea5745086de639bcc8e209
Jiwomdf <mdf.jiwo@gmail.com>
2024-02-20 11:21:58 +0700

    enhance search screens

--- lib/presentation/pages/search/search_screen.dart
+++ lib/presentation/pages/search/search_screen.dart
@@ -12,46 +12,48 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(left: 16, right: 16, top: 32),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 55,
-              child: TextField(
-                onSubmitted: (query) {
-                  Provider.of<MovieSearchNotifier>(context, listen: false)
-                      .fetchMovieSearch(query);
-                },
-                decoration: InputDecoration(
-                  hintText: 'Search title',
-                  prefixIcon: Icon(Icons.search)
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16, top: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 55,
+                child: TextField(
+                  onSubmitted: (query) {
+                    Provider.of<MovieSearchNotifier>(context, listen: false)
+                        .fetchMovieSearch(query);
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Search title',
+                    prefixIcon: Icon(Icons.search)
+                  ),
+                  textInputAction: TextInputAction.search,
                 ),
-                textInputAction: TextInputAction.search,
               ),
-            ),
-            SizedBox(height: 16),
-            Consumer<MovieSearchNotifier>(
-              builder: (context, data, child) {
-                final searchState = data.searchState;
-                return switch (searchState) {
-                  LoadingState<List<Movie>>() => Center(child: CircularProgressIndicator()),
-                  ErrorState<List<Movie>>() => Expanded(child: Container()),
-                  SuccessState<List<Movie>>() => Expanded(
-                  child: ListView.builder(
-                    padding: const EdgeInsets.all(8),
-                      itemBuilder: (context, index) {
-                        final movie = searchState.value[index];
-                        return MovieCard(movie);
-                      },
-                    itemCount: searchState.value.length,
-                  )
-                ),
-                };
-              },
-            ),
-          ],
+              SizedBox(height: 16),
+              Consumer<MovieSearchNotifier>(
+                builder: (context, data, child) {
+                  final searchState = data.searchState;
+                  return switch (searchState) {
+                    LoadingState<List<Movie>>() => Center(child: CircularProgressIndicator()),
+                    ErrorState<List<Movie>>() => Expanded(child: Container()),
+                    SuccessState<List<Movie>>() => Expanded(
+                    child: ListView.builder(
+                      padding: const EdgeInsets.all(8),
+                        itemBuilder: (context, index) {
+                          final movie = searchState.value[index];
+                          return MovieCard(movie);
+                        },
+                      itemCount: searchState.value.length,
+                    )
+                  ),
+                  };
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
